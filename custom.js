253dde8a6a2c84ca219d1c78b2bc0edd010a2b56
Mohamed Hassan <aldirany2016@gmail.com>
2025-09-23 16:23:29 +0300

    upload my first load

--- custom.js
+++ custom.js
@@ -1,1 +1,20 @@
 let content = document.querySelector(".mainContent");
+let total  = 16*16;
+// let width = content.clientWidth;
+// let height = content.clientHeight;
+
+// let cellSize = 10;
+
+// let cols = Math.floor(width / cellSize);
+// let rows = Math.floor(height / cellSize);
+// let total = rows * cols;
+
+for(i=0; i<=total; i++){
+    const grid = document.createElement("div");
+    grid.className ="cell";
+    content.appendChild(grid);
+
+    grid.addEventListener("mouseover", (e)=>{
+        e.target.style.backgroundColor="red"
+    })
+}
